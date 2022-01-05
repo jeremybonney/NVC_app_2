747d473520384e6a3aa381afa405793d3be1eb3f
jeremybonney <89015315+jeremybonney@users.noreply.github.com>
2022-01-05 15:15:20 +0000

    Update first_file.py

--- first_file.py
+++ first_file.py
@@ -1,8 +1,14 @@
-#First file
-
 feelings = ['angry', 'sad', 'happy', 'content']
 
 def feelings_check():
 	print("What are you feeling right now?")
+	print("""
+		a = angry
+		b = sad
+		c = happy
+		d = content""")
+
+	choice = input()
+	print("Ok, you're feeling " + choice + ".")
 
 feelings_check()
