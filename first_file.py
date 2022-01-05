47f9858ac3734753572608348ac43421658cc385
jeremybonney <89015315+jeremybonney@users.noreply.github.com>
2022-01-05 15:22:10 +0000

    Update first_file.py

--- first_file.py
+++ first_file.py
@@ -8,7 +8,15 @@ def feelings_check():
 		c = happy
 		d = content""")
 
-	choice = input()
-	print("Ok, you're feeling " + choice + ".")
+	feeling = input()
+	print("""
+		Ok, you're feeling """ + feeling + ". Anything else? (y/n)")
+	yes_or_no = input()
+	if yes_or_no == 'y':
+		print("What else are you feeling?")
+		feeling = input()
+	else:
+		print("Great. Let's continue.")
+
 
 feelings_check()
