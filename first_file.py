6c8df7dc387144bec255f8cc12258a33bfb93b90
jeremybonney <89015315+jeremybonney@users.noreply.github.com>
2022-01-06 14:06:35 +0000

    Messy update

--- first_file.py
+++ first_file.py
@@ -4,22 +4,53 @@ def feelings_check():
 	
 	feelings =[]
 
-	print("What are you feeling right now?")
-	feeling = input()
+	print("What are you feeling right now? ('n' for nothing or you don't want to share)")
+	
+	first_feeling = input()
+
+	if first_feeling != 'n':
+		feelings.append(first_feeling)
+		print("Ok, you're feeling " + first_feeling + ". And what else? ('n' for nothing or you don't want to share)")
+		checking_feelings = True
+		feeling = input()
+
+		if feeling != 'n':
+			checking_feelings = True
+			while checking_feelings:
+				feeling = input()
+				feelings.append(feeling)
+				print("Ok, you're feeling " + feeling + ". And what else? ('n' for nothing)")
+				more_feelings = input()
+				if more_feelings == 'n':
+					checking_feelings = False
+				else:
+					feeling = input()
+
+	print("Great. This is everything that you're feeling right now:\n")
+	for feeling in feelings:
+		print("• " + feeling)
+	print("\nLet's continue."))
+
+
+
+	else:
+		feelings.append(feeling)
+		print("Great. You've said you're feeling " + first_feeling + ".")
+
 	checking_feelings = True
 	while checking_feelings:
+		feeling = input()
 		feelings.append(feeling)
-		print("""
-			Ok, you're feeling """ + feeling + ". Anything else? (y/n)")
-		yes_or_no = input()
-		if yes_or_no == 'y':
-			print("What else are you feeling?")
-			feeling = input()
-		else:
+		print("Ok, you're feeling " + feeling + ". And what else? ('n' for nothing)")
+		more_feelings = input()
+		if more_feelings == 'n':
 			checking_feelings = False
+		else:
+			feeling = input()
+
 	print("Great. This is everything that you're feeling right now:\n")
-	for feeling_x in feelings:
-		print(feeling_x)
+	for feeling in feelings:
+		print("• " + feeling)
 	print("\nLet's continue.")
 
 feelings_check()
